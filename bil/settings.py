f1e7622340ee42f8861884ad488afb7c0cdfcad9
Atong Abraham Kur <atongkurabraham@gmail.com>
2026-05-09 12:51:44 +0300

    Troubleshooting OTP

--- bil/settings.py
+++ bil/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
 }
 
 # JWT settings
